31d30129e7a9ef5b4e1965efadf90cbbcf295493
pelayi <pelayi@localhost.localdomain>
2015-10-12 21:57:27 -0300

    Updating site

---
+++ public/js/default.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+    $(function () {
+        $.scrollUp({
+            animation: 'fade',
+            //activeOverlay: '#00FFFF',
+            scrollImg: {
+                active: true,
+                //type: 'background',
+                src: '/ars-me-cba/public/img/top.png'
+            }
+        });
+    });
+  
+});
+
